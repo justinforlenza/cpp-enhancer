53b8525aa728c8f178f5c93a6e4bd61e7c1585ad
Justin <jforlenza@steamcenter.nyc>
2025-06-23 13:08:15 -0400

    added school auto select when only one school is tied to account

--- entrypoints/popup.content/index.ts
+++ entrypoints/popup.content/index.ts
@@ -6,27 +6,35 @@ export default defineContentScript({
     '*://careerpathways.nyc/Students/*',
   ],
   main(ctx) {
+    console.debug('[ces-addon] popup: script injected')
+
     const modal = createModalUi(ctx)
 
     modal.mount()
 
     ctx.addEventListener(window, 'click', (event) => {
+      console.debug('[ces-addon] popup: click handled')
+
       if (
         modal.mounted === undefined ||
         !(modal.mounted.firstChild instanceof HTMLDivElement)
       )
         return
 
-      console.debug('ces-addon: student-profile.content: click handled')
+      console.debug('[ces-addon] popup: modal is mounted')
       const target = event.target
 
       if (!(target instanceof HTMLAnchorElement)) return
 
-      console.debug('ces-addon: student-profile.content: is anchor element')
+      console.debug('[ces-addon] popup: is anchor element')
 
-      if (target.innerText !== 'View') return
+      if (!(['View', 'Add New'].includes(target.innerText))) return
 
-      console.debug('ces-addon: student-profile.content: is `View` link')
+      console.debug('[ces-addon] popup: is `View` or `Add New` link')
+
+      if (target.getAttribute('onclick')?.includes('Download')) return
+
+      console.debug('[ces-addon] popup: is not a `Download` link')
 
       const currentTabPane = document.querySelector('.tab-pane.active')
 
@@ -34,23 +42,16 @@ export default defineContentScript({
 
       const originalId = currentTabPane.id
 
-      console.debug(
-        'ces-addon: student-profile.content: current tab pane:',
-        currentTabPane.id,
-      )
+      console.debug('[ces-addon] popup: current tab pane:', currentTabPane.id)
 
       currentTabPane.id = `ces-addon-${originalId}`
-      console.debug(
-        'ces-addon: student-profile.content: changed original tab id',
-      )
+      console.debug('[ces-addon] popup: changed original tab id')
 
       modal.mounted.firstChild.id = originalId
-      console.debug('ces-addon: student-profile.content: updated modal id')
+      console.debug('[ces-addon] popup: updated modal id')
       modal.mounted.showModal()
-      console.debug('ces-addon: student-profile.content: show modal')
-      console.debug(
-        'ces-addon: student-profile.content: finished handling click',
-      )
+      console.debug('[ces-addon] popup: show modal')
+      console.debug('[ces-addon] popup: finished handling click')
     })
   },
 })
